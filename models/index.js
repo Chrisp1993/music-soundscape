c8fbb8c591a9a54663c7137a2222239d3e1ae665
Chrisp1993 <chrispollio93@gmail.com>
2022-07-27 20:50:27 -0400

    fixing post caps

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // import all models
-const Post = require('./Post');
+const Post = require('./fixingcaps');
 const User = require('./User');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
